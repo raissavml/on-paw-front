e616b02d5d8ee98e21ed29eaff0ed29b1aa9f67e
Vinicius David Fonseca <vinicius.davidf@gmail.com>
2023-09-12 22:30:58 -0300

    feat: home page form basic transition

--- src/components/ButtonLink.jsx
+++ src/components/ButtonLink.jsx
@@ -2,30 +2,25 @@ import React from 'react';
 import styled from 'styled-components';
 
 export const Container = styled.button`
-  background: var(--color-primary);
-  color: var(--color-black);
+  color: #286ee2;
   font-weight: 500;
   font-size: 14px;
   opacity: 100%;
 
-  padding: 8px 16px;
-  border-radius: 10px;
-  margin-top: 12px;
+  padding: 8px;
 
   min-width: 120px;
 
   &:hover {
     transition: 300ms;
-    background: var(--color-primary-light);
-    color: var(--color-black);
     cursor: pointer;
   }
 `;
 
-const Button = ({ children, ...rest }) => (
+const ButtonLink = ({ children, ...rest }) => (
   <Container type="button" {...rest}>
     {children}
   </Container>
 );
 
-export default Button;
+export default ButtonLink;
